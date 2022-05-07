4db20806f6263a6f129dcea9058a46ecb0161b5a
Evan Wild <evan@evanwild.ca>
2022-05-07 15:27:17 -0400

    move vars and funcs to object literal

--- russet.js
+++ russet.js
@@ -1,58 +1,155 @@
 const ns = 'http://www.w3.org/2000/svg';
-let root;
-let prevTime;
-let width, height;
 
-const makeCircle = (x, y, r) => {
-  const circle = document.createElementNS(ns, 'circle');
-  circle.setAttribute('cx', x);
-  circle.setAttribute('cy', y);
-  circle.setAttribute('r', r);
-  root.appendChild(circle);
-  return circle;
+const Russet = {
+  root: null,
+  camera: null,
+  world: null,
+  children: [],
+  width: 0,
+  height: 0,
+  prevTime: 0,
+
+  /*
+   * Creates the root svg element that all children will be contained in and
+   * appends it to the DOM
+   */
+  init: function () {
+    this.root = document.createElementNS(ns, 'svg');
+    this.handleResize();
+    document.body.appendChild(this.root);
+
+    // Set up all the listeners
+    window.addEventListener('resize', () => this.handleResize());
+  },
+
+  /*
+   * Starts the animation loop
+   *
+   */
+  startTicking: function () {
+    this.prevTime = performance.now();
+    this.tick();
+  },
+
+  /*
+   * Makes the root svg element take up the full screen, and updates internal
+   * width and height variables
+   *
+   */
+  handleResize: function () {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.root.setAttribute('width', this.width);
+    this.root.setAttribute('height', this.height);
+  },
+
+  /*
+   * An animation loop that is called once every frame, so functionality can be
+   * added with the "onTick" function
+   */
+  tick: function () {
+    // Call "onTick" if the user has defined it
+    if (typeof onTick === 'function') {
+      const deltaTime = (performance.now() - this.prevTime) / 1000;
+      onTick(deltaTime);
+    }
+    this.prevTime = performance.now();
+    window.requestAnimationFrame(() => this.tick());
+  },
+
+  /*
+   * Creates a circle element at a given x, y, and with a given radius, and
+   * appends it to the root svg element
+   */
+  makeCircle: function (x, y, r) {
+    const circle = new Circle(x, y, r);
+    this.root.appendChild(circle.elem);
+    return circle;
+  },
+
+  /*
+   * Creates a line element going from point (x1, y1) to point (x2, y2), and
+   * appends it to the root svg element
+   */
+  makeLine: function (x1, y1, x2, y2) {
+    const line = new Line(x1, y1, x2, y2);
+    this.root.appendChild(line.elem);
+    return line;
+  },
+
+  /*
+   * Sets the active world
+   *
+   */
+  setWorld: function (world) {
+    this.world = world;
+    this.root.style.backgroundColor = world.color;
+  },
+
+  /*
+   * Sets the active camera
+   *
+   */
+  setCamera: function (camera) {
+    this.camera = camera;
+  },
 };
 
-const makeLine = (x1, y1, x2, y2) => {
-  const line = document.createElementNS(ns, 'line');
-  line.setAttribute('x1', x1);
-  line.setAttribute('y1', y1);
-  line.setAttribute('x2', x2);
-  line.setAttribute('y2', y2);
-  line.setAttribute('stroke', 'black');
-  root.appendChild(line);
-  return line;
-};
-
-// Makes the root svg fill the whole screen
-const handleResize = () => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  root.setAttribute('width', width);
-  root.setAttribute('height', height);
-};
-
-// Initializes the root svg element that all others will be children of
-const init = () => {
-  root = document.createElementNS(ns, 'svg');
-  handleResize(); // make it fill screen
-  document.body.appendChild(root);
-};
-
-// Animation loop that gets called every frame
-const tick = () => {
-  if (typeof onTick === 'function') {
-    const deltaTime = (performance.now() - prevTime) / 1000;
-    onTick(deltaTime);
+class World {
+  constructor(width, height, color = 'white') {
+    this.width = width;
+    this.height = height;
+    this.color = color;
   }
-  prevTime = performance.now();
-  window.requestAnimationFrame(tick);
-};
+}
 
-// Set up listeners
-window.addEventListener('resize', handleResize);
+class Camera {
+  constructor(x, y, maxWidth, maxHeight) {
+    this.x = x;
+    this.y = y;
+    this.maxWidth = maxWidth;
+    this.maxHeight = maxHeight;
+  }
+}
 
-init();
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.elem = document.createElementNS(ns, 'circle');
+    this.elem.setAttribute('cx', x);
+    this.elem.setAttribute('cy', y);
+    this.elem.setAttribute('r', r);
+  }
 
-// Start the game loop.
-prevTime = performance.now();
-tick();
+  setX(newX) {
+    this.x = newX;
+    this.elem.setAttribute('cx', newX);
+  }
+
+  setY(newY) {
+    this.y = newY;
+    this.elem.setAttribute('cy', newY);
+  }
+
+  setR(newR) {
+    this.r = newR;
+    this.elem.setAttribute('r', newR);
+  }
+}
+
+class Line {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.elem = document.createElementNS(ns, 'line');
+    this.elem.setAttribute('x1', x1);
+    this.elem.setAttribute('y1', y1);
+    this.elem.setAttribute('x2', x2);
+    this.elem.setAttribute('y2', y2);
+    this.elem.setAttribute('stroke', 'black');
+  }
+}
